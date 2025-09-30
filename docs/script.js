f6fd61ab1da71f22b5a21e191d7d6c90d1011403
Temiii857 <tps5749@psu.edu>
2025-09-30 16:08:18 -0400

    wave effect

--- docs/script.js
+++ docs/script.js
@@ -1,13 +1,13 @@
-const title = document.querySelector("h1");
+const title = document.querySelector("h1"); //Select the <h1> element
 const text = title.textContent;
 title.textContent = "";
 
-
+// loop through each character in the text
 for (let i = 0; i < text.length; i++) {
   let span = document.createElement("span");
   span.textContent = text[i];
-  span.style.display = "inline-block";
-  span.style.animation = `wave 1.5s ease-in-out infinite`;
+  span.style.display = "inline-block"; // makes the letters behave individually
+  span.style.animation = `wave 1.5s ease-in-out infinite`; // applied the wave animation
   span.style.animationDelay = `${i * 0.1}s`;
   title.appendChild(span);
 }
