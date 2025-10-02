6a46b3481572d4acc60e9de67c4fa30d10424b6a
Temiii857 <tps5749@psu.edu>
2025-10-02 16:20:10 -0400

    updated mistakes

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,11 @@
+window.addEventListener('DOMContentLoaded', init, false);
+
+function init() {
 const title = document.querySelector("h1"); //Select the <h1> element
 const text = title.textContent;
 title.textContent = "";
 
+//Wave Effect!
 // loop through each character in the text
 for (let i = 0; i < text.length; i++) {
   let span = document.createElement("span");
@@ -10,4 +14,12 @@ for (let i = 0; i < text.length; i++) {
   span.style.animation = `wave 1.5s ease-in-out infinite`; // applied the wave animation
   span.style.animationDelay = `${i * 0.1}s`;
   title.appendChild(span);
+}
+//Glow Button!
+  function toggleGlow() {
+    title.classList.toggle("glow"); // toggle glow on/off
+  }
+
+
+  title.addEventListener("click", toggleGlow);
 }
